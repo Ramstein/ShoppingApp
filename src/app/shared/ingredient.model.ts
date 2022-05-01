0d225e9ea2f4dc24714f4ac90cbc631c07e18e1d
MD.Zeeshan <gerialworld@gmail.com>
2022-05-01 20:06:51 +0530

    UI changes done

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,8 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+  // for directly creating the variable in constructor, use public
+
+  constructor(public name: string, public amount: number) {
+  }
+}
